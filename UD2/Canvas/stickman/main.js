91c1b354922c9355f81a0d2540e682c8b187fe4e
Aarón Durán <aaronduranwebdev@gmail.com>
2022-10-28 08:51:57 +0200

    Stickman

--- UD2/Canvas/stickman/main.js
+++ UD2/Canvas/stickman/main.js
@@ -21,18 +21,28 @@ function linea(x, y, posX, posY, ancho, color) {
   ctx.stroke();
   ctx.closePath();
 }
-function dibujarStickman() {
+function dibujarStickman(brazos = false) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   circulo("white", "white", 1, 100, 50, 30);
-  circulo("#c00", "", 3, 100, 50, 20, 1);
+  if (!brazos) {
+    circulo("#c00", "", 3, 100, 50, 20, 1);
+  } else {
+    circulo("#c00", "#c00", 1, 100, 60, 10, 0);
+  }
+
   circulo("", "red", 3, 90, 45, 3);
   circulo("", "red", 3, 110, 45, 3);
   // Tronco
   linea(canvas.width / 2, 80, canvas.width / 2, canvas.height - 80, 6, "white");
   // Brazos
-  linea(canvas.width / 2, 90, canvas.width / 2 - 30, canvas.height / 2 - 10, 6, 'white');
-  linea(canvas.width / 2, 90, canvas.width / 2 + 30, canvas.height / 2 - 10, 6, 'white');
+  if (!brazos) {
+    linea(canvas.width / 2, 90, canvas.width / 2 - 30, canvas.height / 2 - 10, 6, 'white');
+    linea(canvas.width / 2, 90, canvas.width / 2 + 30, canvas.height / 2 - 10, 6, 'white');
+  } else {
+    linea(canvas.width / 2, 90, canvas.width / 2 - 40, canvas.height / 2 - 70, 6, 'white');
+    linea(canvas.width / 2, 90, canvas.width / 2 + 40, canvas.height / 2 - 70, 6, 'white');
+  }
   // Piernas
   linea(canvas.width / 2, canvas.height - 80, canvas.width / 2 - 30, canvas.height / 2 + 130, 6, 'white');
   linea(canvas.width / 2, canvas.height - 80, canvas.width / 2 + 30, canvas.height / 2 + 130, 6, 'white');
 }
-
